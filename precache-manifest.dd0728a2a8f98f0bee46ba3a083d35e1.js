17869b9f71b79557e61894f3ef3ee10405a6e509
DESKTOP-OBQC0DF\HUNG PC <linh.mhl9@gmail.com>
2021-03-18 23:12:10 +0700

    Updates

---
+++ precache-manifest.dd0728a2a8f98f0bee46ba3a083d35e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e261d6dd6378787da674d6fdb502cc35",
+    "url": "/mhlinh-reactjs-resume/index.html"
+  },
+  {
+    "revision": "26df3c0e0514c78ab5e6",
+    "url": "/mhlinh-reactjs-resume/static/css/2.49ecd109.chunk.css"
+  },
+  {
+    "revision": "68d82a9dec64bd18040e",
+    "url": "/mhlinh-reactjs-resume/static/css/main.345f640c.chunk.css"
+  },
+  {
+    "revision": "26df3c0e0514c78ab5e6",
+    "url": "/mhlinh-reactjs-resume/static/js/2.2dbb8040.chunk.js"
+  },
+  {
+    "revision": "39cf7a9a3fca6bbc6cc997fb6ffa9cd6",
+    "url": "/mhlinh-reactjs-resume/static/js/2.2dbb8040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d82a9dec64bd18040e",
+    "url": "/mhlinh-reactjs-resume/static/js/main.da57376c.chunk.js"
+  },
+  {
+    "revision": "b89ee157af106e7e5296",
+    "url": "/mhlinh-reactjs-resume/static/js/runtime-main.6ca87725.js"
+  }
+]);
